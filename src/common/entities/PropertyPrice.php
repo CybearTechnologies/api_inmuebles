f558d0e994b57176a736a94066460a54b52539e4
ramiroavch <ramiroavch@gmail.com>
2020-01-16 22:52:49 -0400

    aded SP insertPP

--- src/common/entities/PropertyPrice.php
+++ src/common/entities/PropertyPrice.php
@@ -3,7 +3,6 @@ class PropertyPrice extends Entity {
 	private $_price;
 	private $_date;
 	private $_final;
-	private $_property;
 
 	/**
 	 * PropertyPrice constructor.
--- src/data_access/Dao/DaoPropertyPrice.php
+++ src/data_access/Dao/DaoPropertyPrice.php
@@ -1,7 +1,7 @@
 <?php
 class DaoPropertyPrice extends Dao {
 	//insertpropertyprice(price double(20,2), final tinyint,property int,user int)
-	private const QUERY_CREATE = "CALL insertPropertyPrice(:price,:final,:property,:user)";
+	private const QUERY_CREATE = "CALL insertpropertyprice(:price,:final,:property,:user)";
 	private const QUERY_GET_ALL = "";
 	private const QUERY_GET_BY_ID = "CALL getpropertypricebyid(:id)";
 	private const QUERY_GET_PRICE_BY_PROPERTY_ID = "CALL getAllPricesByProperty(:id)";
