75f2ca350e219f039a8179e5dd101a3c22ce46f0
jafjdev <jafjdev@gmail.com>
2020-01-20 02:04:27 -0400

    [DAO/COMMAND] Created command & dao methods @pLAN

---
+++ src/logic/Plan/ActivePlanCommand.php
@@ -0,0 +1,10 @@
+<?php
+class ActivePlanCommand extends Command {
+	public function execute ():void {
+		// TODO: Implement execute() method.
+	}
+
+	public function return () {
+		// TODO: Implement return() method.
+	}
+}
