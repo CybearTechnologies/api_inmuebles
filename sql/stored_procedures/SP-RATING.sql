f00d4a18e2a0a67af31af4b5e72db24b3c3eeec7
ramiroavch <ramiroavch@gmail.com>
2020-01-24 00:30:03 -0400

    [DaoRating] added methods
    missing create method

--- sql/stored_procedures/SP-RATING.sql
+++ sql/stored_procedures/SP-RATING.sql
@@ -5,10 +5,12 @@
  */
 DROP PROCEDURE IF EXISTS insertRating;
 DELIMITER $$
-CREATE PROCEDURE insertRating(score float, message varchar(200), user_target int, user_fk int)
+CREATE PROCEDURE insertRating(score float, message varchar(200), user_target int, user_fk int,dateCreated DATE,
+dateModified DATE )
 BEGIN
-    INSERT INTO rating(ra_score, ra_message, ra_user_fk, ra_user_created_fk, ra_user_modified_fk)
-    VALUES (score, message, user_target, user_fk, user_fk);
+    INSERT INTO rating(ra_score, ra_message, ra_user_fk, ra_user_created_fk, ra_user_modified_fk,ra_date_created,
+    ra_date_modified)
+    VALUES (score, message, user_target, user_fk, user_fk, dateCreated,dateModified);
     SELECT ra_id id,
            ra_score score,
            ra_message message,
@@ -75,6 +77,22 @@ BEGIN
            ra_date_modified dateModified
     FROM rating
     WHERE ra_user_fk = id_user;
+END$$
+
+DROP PROCEDURE IF EXISTS getAllRating;
+DELIMITER $$
+CREATE PROCEDURE getAllRating()
+BEGIN
+    SELECT ra_id id,
+           ra_score score,
+           ra_message message,
+           ra_active active,
+           ra_deleted 'delete',
+           ra_user_created_fk userCreator,
+           ra_user_modified_fk userModifier,
+           ra_date_created dateCreated,
+           ra_date_modified dateModified
+    FROM rating;
 END$$
 /**
  ----------------------------------------------------------------------------------------------------------------------
--- src/data_access/Dao/DaoRating.php
+++ src/data_access/Dao/DaoRating.php
@@ -3,6 +3,8 @@ class DaoRating extends Dao {
 	private const QUERY_CREATE = "CALL insertRating(:score,:message,:user)"; //TODO
 	private const QUERY_GET_BY_ID = "CALL getRatingById(:id)";
 	private const QUERY_GET_ALL_RATING_BY_USER = "CALL getAllRatingByUser(:id)";
+	private const QUERY_DELETE = "CALL deleteRating";
+	private const QUERY_GET_ALL = "CALL getAllRating()";
 	private $_entity;
 
 	/**
@@ -62,13 +64,56 @@ class DaoRating extends Dao {
 	}
 
 	/**
+	 * @return Rating
+	 * @throws DatabaseConnectionException
+	 * @throws RatingNotFoundException
+	 */
+	public function deleteRating () {
+		try {
+			$id = $this->_entity->getId();
+			$user = $this->_entity->getUserModifier();
+			$stmt = $this->getDatabase()->prepare(self::QUERY_DELETE);
+			$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+			$stmt->bindParam(":user", $user, PDO::PARAM_INT);
+			$stmt->execute();
+			return $this->extract($stmt->fetch(PDO::FETCH_OBJ));
+		}
+		catch (PDOException $exception) {
+			Logger::error($exception, Logger::ERROR);
+			Throw new DatabaseConnectionException("Database connection problem.", 500);
+		}
+	}
+
+	/**
+	 * @return array
+	 * @throws DatabaseConnectionException
+	 * @throws RatingNotFoundException
+	 */
+	public function getAllRating () {
+		try {
+			$stmt = $this->getDatabase()->prepare(self::QUERY_GET_ALL);
+			$stmt->execute();
+			if ($stmt->rowCount() == 0)
+				Throw new RatingNotFoundException("There are no Rating found", 200);
+			else {
+				return $this->extractAll($stmt->fetchAll(PDO::FETCH_OBJ));
+			}
+		}
+		catch (PDOException $exception) {
+			Logger::error($exception, Logger::ERROR);
+			Throw new DatabaseConnectionException("Database connection problem.", 500);
+		}
+	}
+
+
+	/**
 	 * @param $dbObject
 	 *
 	 * @return Rating
 	 */
 	protected function extract ($dbObject) {
 		return FactoryEntity::createRating($dbObject->id, $dbObject->score, $dbObject->message, $dbObject->active,
-			$dbObject->delete, $dbObject->userCreator, $dbObject->userModifier, $dbObject->dateCreated,
+ 			$dbObject->delete, $dbObject->userCreator, $dbObject->userModifier, $dbObject->dateCreated,
 			$dbObject->dateMotified);
 	}
 }
