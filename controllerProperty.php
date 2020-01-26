bebacae162a926955a43185e0a9aa58c26ebfa83
jafjdev <jafjdev@gmail.com>
2020-01-26 11:13:15 -0400

    [Property]fixed

--- controllerProperty.php
+++ controllerProperty.php
@@ -5,50 +5,50 @@ $get = Tools::getObject();
 $return = null;
 $mapper = FactoryMapper::createMapperProperty();
 $mapperExtra = FactoryMapper::createMapperExtra();
-$mapperPropertyPrice = FactoryMapper::createMapperProperty();
+$mapperPropertyPrice = FactoryMapper::createMapperPropertyPrice();
 $property = FactoryEntity::createProperty(0);
 switch ($_SERVER["REQUEST_METHOD"]) {
 	case "GET":
 		if (isset($get->id) && is_numeric($get->id)) {
-			/*			$property->setId($get->id);
-						$command = FactoryCommand::createGetPropertyByIdCommand($property);
-						try {
-							$command->execute();
-							$dto = $mapper->fromEntityToDTO($command->return());
-							if (isset($get->extras)) {
-								$command = FactoryCommand::createGetAllExtrasByPropertyIdCommand($property);
-								$dto->extras = $mapperExtra->fromEntityArrayToDtoArray($command->return());
-								try {
-									$command->execute();
-									if (isset($get->price)) {
-										$command = FactoryCommand::createGetPropertyPriceByPropertyIdCommand($property);
-										try {
-											$command->execute();
-											$dto->price = $mapperPropertyPrice->fromEntityArrayToDtoArray($command->return());
-										}
-										catch (InvalidPropertyPriceException $exception) {
-											unset($dto->price);
-										}
-									}
-								}
-								catch (ExtraNotFoundException $exception) {
-									unset($dto->extras);
-								}
+			$property->setId($get->id);
+			$command = FactoryCommand::createGetPropertyByIdCommand($property);
+			try {
+				$command->execute();
+				$dto = $mapper->fromEntityToDTO($command->return());
+				if (isset($get->extras)) {
+					$command = FactoryCommand::createGetAllExtrasByPropertyIdCommand($property);
+					try {
+						$command->execute();
+						$dto->extras = $mapperExtra->fromEntityArrayToDtoArray($command->return());
+						if (isset($get->price)) {
+							$command = FactoryCommand::createGetPropertyPriceByPropertyIdCommand($property);
+							try {
+								$command->execute();
+								$dto->price = $mapperPropertyPrice->fromEntityArrayToDtoArray($command->return());
 							}
-							else
-								unset($dto->seats);
-							$return = $dto;
-							Tools::setResponse();
+							catch (InvalidPropertyPriceException $exception) {
+								unset($dto->price);
+							}
 						}
-						catch (DatabaseConnectionException $exception) {
-							$return = new ErrorResponse(Values::getText("ERROR_DATABASE"));
-							Tools::setResponse($exception->getCode());
-						}
-						catch (PropertyNotFoundException $exception) {
-							$return = new ErrorResponse(Values::getText("ERROR_PROPERTY_NOT_FOUND"));
-							Tools::setResponse($exception->getCode());
-						}
-						echo json_encode($return);*/
+					}
+					catch (ExtraNotFoundException $exception) {
+						unset($dto->extras);
+					}
+				}
+				else
+					unset($dto->seats);
+				$return = $dto;
+				Tools::setResponse();
+			}
+			catch (DatabaseConnectionException $exception) {
+				$return = new ErrorResponse(Values::getText("ERROR_DATABASE"));
+				Tools::setResponse($exception->getCode());
+			}
+			catch (PropertyNotFoundException $exception) {
+				$return = new ErrorResponse(Values::getText("ERROR_PROPERTY_NOT_FOUND"));
+				Tools::setResponse($exception->getCode());
+			}
+			echo json_encode($return);
 		}
 		else {
 			$command = FactoryCommand::createGetAllPropertyCommand();
