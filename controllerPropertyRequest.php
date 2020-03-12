715599c3853fcdef9ab982a3a9fe22fb4df57389
jafjdev <jafjdev@gmail.com>
2020-03-12 10:43:07 -0400

    [PropertyRequest] controllerPropertyRequest

---
+++ controllerPropertyRequest.php
@@ -0,0 +1,36 @@
+<?php
+require_once "autoload.php";
+Tools::headers();
+$get = Tools::getObject();
+$return = null;
+$mapper = FactoryMapper::createMapperRequest();
+switch ($_SERVER["REQUEST_METHOD"]) {
+	case "GET":
+		echo json_encode($return);
+		break;
+	case "POST":
+		if (isset($post->property) && is_numeric($post->property) && isset($post->user) && is_numeric($post->user)) {
+			try {
+				$command = FactoryCommand::createCommandCreateRequest(
+					FactoryEntity::createRequest(-1, $post->property, $post->user));
+				$command->execute();
+				$return = $mapper->fromEntityToDto($command->return());
+				Tools::setResponse();
+			}
+			catch (DatabaseConnectionException $e) {
+				$return = new ErrorResponse(Values::getText("ERROR_DATABASE"));
+				Tools::setResponse(Values::getValue("ERROR_DATABASE"));
+			}
+		}
+		echo json_encode($return);
+		break;
+	case "PUT":
+		echo json_encode($return);
+		break;
+	case "DELETE":
+		echo json_encode($return);
+		break;
+	default:
+		Tools::setResponse(405);
+		break;
+}
