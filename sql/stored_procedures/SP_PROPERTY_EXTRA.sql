66bdcb2fc127e12e6ea878663bd56b8eb1f66b79
RAM <ramirovargas.dev@gmail.com>
2020-03-29 20:28:00 -0400

    agregando sp de get access by rol

--- sql/stored_procedures/SP_PROPERTY_EXTRA.sql
+++ sql/stored_procedures/SP_PROPERTY_EXTRA.sql
@@ -10,22 +10,17 @@ BEGIN
         VALUES (amount, propertyId, extraId, user,user,datecreated,datecreated);
     END IF;
     SELECT pe.pe_id id,
-           ex.ex_name name,
-           ex.ex_active active,
+           pe.pe_active active,
            pe.pe_value value,
-           ex.ex_id extra,
-           ex.ex_deleted 'delete',
-           ex_icon icon,
+           pe.pe_extra_fk extra,
+           pe.pe_deleted 'delete',
            pe.pe_property_fk property,
-           ex.ex_user_created_fk userCreator,
-           ex.ex_date_created dateCreated,
-           ex.ex_user_modified_fk userModifier,
-           ex.ex_date_modified dateModified
-    FROM extra ex,
-         property_extra pe
-    WHERE pe.pe_extra_fk = ex_id
-      AND pe.pe_property_fk = propertyid
-      AND pe.pe_deleted = 0
+           pe.pe_user_created_fk userCreator,
+           pe.pe_date_created dateCreated,
+           pe.pe_user_modified_fk userModifier,
+           pe.pe_date_modified dateModified
+    FROM   property_extra pe
+    WHERE pe.pe_deleted = 0
       AND pe.pe_id = last_insert_id();
 END$$
 
@@ -34,21 +29,17 @@ DELIMITER $$
 CREATE PROCEDURE getPropertyExtraByPropertyId(id_pr int)
 BEGIN
     SELECT pe.pe_id id,
-           ex.ex_name name,
-           ex.ex_active active,
+           pe.pe_active active,
            pe.pe_value value,
-           ex.ex_id extra,
-           ex.ex_deleted 'delete',
-           ex_icon icon,
+           pe.pe_extra_fk extra,
+           pe.pe_deleted 'delete',
            pe.pe_property_fk property,
-           ex.ex_user_created_fk userCreator,
-           ex.ex_date_created dateCreated,
-           ex.ex_user_modified_fk userModifier,
-           ex.ex_date_modified dateModified
-    FROM extra ex,
-         property_extra pe
-    WHERE pe.pe_extra_fk = ex_id
-      AND pe.pe_property_fk = id_pr
+           pe.pe_user_created_fk userCreator,
+           pe.pe_date_created dateCreated,
+           pe.pe_user_modified_fk userModifier,
+           pe.pe_date_modified dateModified
+    FROM   property_extra pe
+    WHERE  pe.pe_property_fk = id_pr
       AND pe.pe_deleted = 0;
 END$$
 
