d025340b6a7f4cc709c415b20fd68e20843b8c33
jafjdev <jafjdev@gmail.com>
2020-04-19 01:54:28 -0400

    [chore] subscription post now needs only application header

--- controllerSubscription.php
+++ controllerSubscription.php
@@ -78,84 +78,61 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 		echo json_encode($return);
 		break;
 	case "POST":
-		if (Validate::headers()) {
-			try {
-				$loggedUser = Tools::getUserLogged($headers[Values::BEARER_HEADER],
-					$headers[Values::APPLICATION_HEADER]);
-				if (Validate::subscription($post)) {
-					try {
-						// Files processing
-						$details = [];
-						$files = [];
-						if (isset($_FILES['files']) && count($_FILES['files']['size']) > 0) {
-							$i = 0;
-							foreach ($_FILES['files']['name'] as $file) {
-								$path = Tools::saveFile($file, $_FILES['files']['tmp_name'][$i],
-									$post->firstName . $post->lastName, 'files/user');
-								array_push($details,
-									FactoryDto::createDtoSubscriptionDetail(-1, Environment::baseURL() . $path, -1));
-								array_push($files, __DIR__ . '/' . $path);
-								$i++;
-							}
+		if (Validate::application()) {
+			if (Validate::subscription($post)) {
+				try {
+					// Files processing
+					$details = [];
+					$files = [];
+					if (isset($_FILES['files']) && count($_FILES['files']['size']) > 0) {
+						$i = 0;
+						foreach ($_FILES['files']['name'] as $file) {
+							$path = Tools::saveFile($file, $_FILES['files']['tmp_name'][$i],
+								$post->firstName . $post->lastName, 'files/user');
+							array_push($details,
+								FactoryDto::createDtoSubscriptionDetail(-1, Environment::baseURL() . $path, -1));
+							array_push($files, __DIR__ . '/' . $path);
+							$i++;
 						}
-						$post->password = Validate::passwordHash($post->password . Environment::siteKey() . Tools::siteEncrypt($post->password));
-						/** @var DtoSubscription $post */
-						$subscription = $mapper->fromDtoToEntity($post);
-						$command = FactoryCommand::createCommandSubscribeUser($subscription, $details);
-						$command->execute();
-						$return = $command->return();
-						Tools::setResponse();
-						$wrapper->setFrom()->setTo($return->email, $return->firstName . ' ' . $return->lastName)
-							->setSubject('Buscamatch')
-							->setBody('Se ha enviado tu solicitud correctamente. Pronto se dara respuesta!')
-							->sendEmail();
 					}
-					catch (DatabaseConnectionException $exception) {
-						foreach ($files as $file)
-							file_exists($file) ? Tools::removeFile($file) : null;
-						$return = new ErrorResponse($exception->getMessage());
-						Tools::setResponse(Values::getValue("ERROR_DATABASE"));
-					}
-					catch (FileNotFoundException $exception) {
-						foreach ($files as $file)
-							file_exists($file) ? Tools::removeFile($file) : null;
-						$return = $exception->getMessage();
-						Tools::setResponse($exception->getCode());
-					}
-					catch (SaveFileException $exception) {
-						foreach ($files as $file)
-							file_exists($file) ? Tools::removeFile($file) : null;
-						$return = $exception->getMessage();
-						Tools::setResponse($exception->getCode());
-					}
-					catch (MailerException $exception) {
-						$return = new ErrorResponse($exception->getMessage());
-						Tools::setResponse(Values::getValue("ERROR_MAILER"));
-					}
+					$post->password = Validate::passwordHash($post->password . Environment::siteKey() . Tools::siteEncrypt($post->password));
+					/** @var DtoSubscription $post */
+					$subscription = $mapper->fromDtoToEntity($post);
+					$command = FactoryCommand::createCommandSubscribeUser($subscription, $details);
+					$command->execute();
+					$return = $command->return();
+					Tools::setResponse();
+					$wrapper->setFrom()->setTo($return->email, $return->firstName . ' ' . $return->lastName)
+						->setSubject('Buscamatch')
+						->setBody('Se ha enviado tu solicitud correctamente. Pronto se dara respuesta!')
+						->sendEmail();
 				}
-				else {
-					$return = new ErrorResponse(Values::getText("ERROR_DATA_INCOMPLETE"));
-					Tools::setResponse(Values::getValue("ERROR_DATA_INCOMPLETE"));
+				catch (DatabaseConnectionException $exception) {
+					foreach ($files as $file)
+						file_exists($file) ? Tools::removeFile($file) : null;
+					$return = new ErrorResponse($exception->getMessage());
+					Tools::setResponse(Values::getValue("ERROR_DATABASE"));
+				}
+				catch (FileNotFoundException $exception) {
+					foreach ($files as $file)
+						file_exists($file) ? Tools::removeFile($file) : null;
+					$return = $exception->getMessage();
+					Tools::setResponse($exception->getCode());
+				}
+				catch (SaveFileException $exception) {
+					foreach ($files as $file)
+						file_exists($file) ? Tools::removeFile($file) : null;
+					$return = $exception->getMessage();
+					Tools::setResponse($exception->getCode());
+				}
+				catch (MailerException $exception) {
+					$return = new ErrorResponse($exception->getMessage());
+					Tools::setResponse(Values::getValue("ERROR_MAILER"));
 				}
 			}
-			catch (DatabaseConnectionException $e) {
-				$return = new ErrorResponse(Values::getText("ERROR_DATABASE"));
-				Tools::setResponse(Values::getValue("ERROR_DATABASE"));
-			}
-			catch (OriginNotFoundException $exception) {
-				Logger::exception($exception, Logger::ERROR);
-				$return = new ErrorResponse(Values::getText('ERROR_ORIGIN_NOT_FOUND'));
-				Tools::setResponse(Values::getValue('ERROR_ORIGIN_NOT_FOUND'));
-			}
-			catch (InvalidJWTException $exception) {
-				Logger::exception($exception, Logger::ERROR);
-				$return = new ErrorResponse($exception->getMessage());
-				Tools::setResponse(Values::getValue('ERROR_LOGIN_USER_NOT_LOGGED'));
-			}
-			catch (Exception $exception) {
-				Logger::exception($exception, Logger::ERROR);
-				$return = new ErrorResponse($exception->getMessage());
-				Tools::setResponse(Values::getValue('ERROR_LOGIN_USER_NOT_LOGGED'));
+			else {
+				$return = new ErrorResponse(Values::getText("ERROR_DATA_INCOMPLETE"));
+				Tools::setResponse(Values::getValue("ERROR_DATA_INCOMPLETE"));
 			}
 		}
 		else {
