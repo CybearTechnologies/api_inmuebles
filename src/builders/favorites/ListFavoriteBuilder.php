6abc5558b8aa29df87b79bc8727c1d2aa84f2ae7
José Cedeño <jafjdev@gmail.com>
2020-05-25 21:17:32 -0400

    [feature] User favorites now returns in property, user creator.

--- src/builders/favorites/ListFavoriteBuilder.php
+++ src/builders/favorites/ListFavoriteBuilder.php
@@ -43,7 +43,8 @@ class ListFavoriteBuilder extends ListBuilder {
 					->withType()
 					->withLocation()
 					->withExtras()
-					->clean()
+					->withUserDetail()
+					->unsetUserModifier()
 					->build();
 			}
 			catch (PropertyNotFoundException $e) {
