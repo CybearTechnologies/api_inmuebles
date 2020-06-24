a03c7e1315d0194ca1b49c281efb88d137871a75
RAM <ramirovargas.dev@gmail.com>
2020-06-24 02:08:23 -0400

    [Feat] added docs

--- src/logic/Subscription/CommandGetAllSubscription.php
+++ src/logic/Subscription/CommandGetAllSubscription.php
@@ -11,6 +11,7 @@ class CommandGetAllSubscription extends Command {
 	}
 
 	/**
+	 * @throws AgencyNotFoundException
 	 * @throws DatabaseConnectionException
 	 * @throws SubscriptionNotFoundException
 	 */
